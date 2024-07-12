c84d42bb0ae31682df97ff0d9c5f4973434e7a29
chendening <chendening@skyworth.com>
2024-07-12 10:54:49 +0800

    add input

--- kernel/4 input/input-test.c
+++ kernel/4 input/input-test.c
@@ -1,116 +1,35 @@
-#include <linux/module.h>
-#include <linux/poll.h>
-#include <linux/fs.h>
-#include <linux/errno.h>
-#include <linux/miscdevice.h>
-#include <linux/kernel.h>
-#include <linux/major.h>
-#include <linux/mutex.h>
-#include <linux/proc_fs.h>
-#include <linux/seq_file.h>
-#include <linux/stat.h>
 #include <linux/init.h>
-#include <linux/device.h>
-#include <linux/tty.h>
-#include <linux/kmod.h>
-#include <linux/gfp.h>
-#include <linux/gpio/consumer.h>
-#include <linux/platform_device.h>
-#include <linux/of_gpio.h>
-#include <linux/of_irq.h>
-#include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/slab.h>
-#include <linux/fcntl.h>
-#include <linux/timer.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/printk.h>  /* pr_info(() */
+#include <linux/delay.h>  /* msleep() */
+#include <linux/version.h> /* Linux vresion */
+
 #include <linux/input.h>
-/* https://blog.csdn.net/lihuan680680/article/details/121325980 by 面朝大海0902*/
-struct gpio_key{
-    int gpio;
-    struct gpio_desc *gpiod;
-    int flag;
-    int irq;
-};
+#include <linux/input-event-codes.h>
 
 struct input_dev *key_input;
-static struct gpio_key *ptr_gpio_key;
-struct timer_list key_timer;
+static struct hrtimer my_timer;
 
-void timer_function(unsigned long arg)
+enum hrtimer_restart my_timer_callback(struct hrtimer *timer)
 {
-    int value;
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    struct gpio_key *ptr_gpio_key_temp = arg;
-    
-    if(ptr_gpio_key_temp == NULL)
-        return;
-    
-    value = gpiod_get_value(ptr_gpio_key_temp->gpiod);
-
-    if(value == 0)
-    {
-        if(ptr_gpio_key_temp->gpio == ptr_gpio_key[0].gpio)
-        {
-            input_report_key(key_input, KEY_L, 1);
-        }
-        else
-        {
-            input_report_key(key_input, KEY_S, 1);
-        }
-    }
-    else
-    {
-        if(ptr_gpio_key_temp->gpio == ptr_gpio_key[0].gpio)
-        {
-            input_report_key(key_input, KEY_L, 0);
-        }
-        else
-        {
-            input_report_key(key_input, KEY_S, 0);
-        }
-    }
-    input_sync(key_input);
+    /* 在这里执行定时操作 */
+    /* 返回 HRTIMER_NORESTART 表示定时器不需要重新启动，返回 HRTIMER_RESTART 表示需要重新启动定时器 */
+    input_report_key(key_input, KEY_S, 1);
+	input_sync(key_input);
+    hrtimer_forward_now(timer, ms_to_ktime(1000)); //HRTIMER_RESTART需要这条语句
+    return HRTIMER_RESTART;
 }
 
-static irqreturn_t gpio_key_isr(int irq, void *dev_id)
+int input_init(void)
 {
-    key_timer.data =(unsigned long) dev_id;
-    mod_timer(&key_timer, jiffies + HZ/4);
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    return IRQ_HANDLED;
-}
-
-static int key_probe(struct platform_device *pdev)
-{
-    int count = 0;
-    int i=0;
-    enum of_gpio_flags flag;
-    struct device_node *node = pdev->dev.of_node;
-    
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    count = of_gpio_count(node);
-    ptr_gpio_key = kzalloc(sizeof(struct gpio_key)*count, GFP_KERNEL);
-    
-    for(i=0;i<count;i++)
-    {
-        ptr_gpio_key[i].gpio = of_get_gpio_flags(node, i, &flag);
-        if(ptr_gpio_key[i].gpio < 0)
-        {
-            printk(KERN_ERR "of_get_gpio_flags is err\r\n");
-        }
-        else
-        {
-            printk(KERN_INFO "gpio num is %d\r\n",ptr_gpio_key[i].gpio);
-        }
-        ptr_gpio_key[i].gpiod = gpio_to_desc(ptr_gpio_key[i].gpio);
-        ptr_gpio_key[i].flag = flag & OF_GPIO_ACTIVE_LOW;
-        ptr_gpio_key[i].irq = gpio_to_irq(ptr_gpio_key[i].gpio);
-        request_irq(ptr_gpio_key[i].irq, gpio_key_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "gpio_key", &ptr_gpio_key[i]);
-    }
-
-    /* 1、申请 input_dev */
+    int result = 0;
+	ktime_t duration;
+	printk("chendening module init \n");
+	
+	/* 1、申请 input_dev */
     key_input = input_allocate_device();
-    key_input->name = "key_input";
+    key_input->name = "chendening_key_input";
     
     /* 2、初始化 input_dev，设置产生哪些事件 */
     __set_bit(EV_KEY, key_input->evbit);
@@ -121,77 +40,25 @@ static int key_probe(struct platform_device *pdev)
     __set_bit(KEY_S, key_input->keybit);
 
     /* 4、注册输入设备 */
-    input_register_device(key_input);
-    
-    init_timer(&key_timer);
-    key_timer.function = timer_function;
-    key_timer.expires = ~0;
-    //add_timer(&key_timer);
+    if(input_register_device(key_input) < 0) {
+		printk("input_register_device error.\n");
+		result = -1;
+	}
+	
+	duration = ktime_set(1, 0);  // 设置定时器超时时间为 1 秒
+	hrtimer_init(&my_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);  
+    my_timer.function = my_timer_callback;
+    hrtimer_start(&my_timer, duration, HRTIMER_MODE_REL);
 
-    return 0;    
-}
-
-
-
-static int key_remove(struct platform_device *pdev)
-{
-    int count = 0;
-    int i = 0;
-    struct device_node *node = pdev->dev.of_node;
-
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    count = of_gpio_count(node);
-
-    for(i=0;i<count;i++)
-    {
-        free_irq(ptr_gpio_key[i].irq, &ptr_gpio_key[i]);
-    }
-
-    kfree(ptr_gpio_key);
-    del_timer(&key_timer);
-
-    /* 释放 input_dev */
-    input_unregister_device(key_input);
-    input_free_device(key_input);
-
-    return 0;    
-}
-
-
-static const struct of_device_id my_key[] =
-{
-    {.compatible = "my,key_driver"},
-    {},
-};
-
-
-static struct platform_driver key_driver =
-{
-    .probe  = key_probe,
-    .remove = key_remove,
-    .driver = 
-    {
-        .name = "key_gpio",
-        .of_match_table = my_key,
-    },
-};
-
-
-static int __init my_key_init(void)
-{
-    int result;
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    result = platform_driver_register(&key_driver);
     return result;
 }
 
-
-static void __exit my_key_exit(void)
+void input_exit(void)
 {
-    printk(KERN_INFO "%s %s line is %d \r\n", __FILE__, __FUNCTION__, __LINE__);
-    platform_driver_unregister(&key_driver);
+	printk("chendening module exit \n");
 }
 
-module_init(my_key_init);
-module_exit(my_key_exit);
+module_init(input_init);
+module_exit(input_exit);
+MODULE_DESCRIPTION("chendening");
 MODULE_LICENSE("GPL");
