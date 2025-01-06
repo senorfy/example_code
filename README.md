1089496a21948224c2b67386e96be1018e961988
chendening <chendening@skyworth.com>
2025-01-06 10:23:10 +0000

    changge

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 编译user程序
-使用 ./make build <id>编译user目录下的程序
+使用 ./make build <id>编译user目录下的程序  
 使用 ./make clean清除所有的build文件夹
---
+++ user/8_加载动态库/cdnprint.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <net/if.h>
+#include <cstring>
+
+void cdn_printf_hello_world()
+{
+    std::cout << "cdn printf hello world" << std::endl;
+}
